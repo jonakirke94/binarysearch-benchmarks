bb3cbbbd775dcc075d81801ccf81d3cc78ce2d45
DESKTOP-JRQFD6G\inter <jonakirke94@gmail.com>
2019-02-19 17:31:31 +0100

    fixed benchmark pitfalls with explanation

--- binarysearch-benchmarks/Program.cs
+++ binarysearch-benchmarks/Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,18 +10,23 @@ namespace binarysearch_benchmarks
 {
     public class Program
     {
+        private static double result;
+        private static readonly int[] list = { 2, 6, 8, 12, 15, 16, 22, 44, 74 };
+        private static readonly int key = 15;
 
         public static int NumberOfTimesCalled = 0;
         static void Main(string[] args)
         {
-            var numberList = new int[] { 2, 6, 8, 12, 15, 16, 22, 44, 74 };
-            var key = 15;
-            BenchmarkIterative(numberList, key);
-            BenchmarkRecursive(numberList, key);
-            Console.ReadKey();
+           var iterations = 1000000000;
+           Benchmark("Iterative", iterations, IterativeSearch);
+           Benchmark("Recursive", iterations, RecursiveSearch);
+           Console.ReadKey();
+
+           // If the compiler has tail-call optimization then the iterative and recursive approach
+           // will be equally fast. The .NET/C# compiler does NOT so the iterative is slightly faster.
         }
 
-        static object IterativeSearch(int[] list, int key)
+        static double IterativeSearch(int[] list, int key)
         {
             var min = 0;
             var max = list.Length - 1;
@@ -31,7 +37,7 @@ namespace binarysearch_benchmarks
 
                 if (list[mid] == key)
                 {
-                    return mid;
+                    return ++mid;
                 }
 
                 else if (key < list[mid])
@@ -45,71 +51,110 @@ namespace binarysearch_benchmarks
                 }
             }
 
-            return "Not Found";
+            //return "Not Found";
+            return 0;
         }
 
-
-        static void BenchmarkIterative(int[] numberList, int key)
-        {
-            var stopwatch = new Stopwatch();
-            var n = 1000000;
-            var ticks = 0d;
-
-            for (int i = 0; i < n; i++)
-            {
-                stopwatch.Reset();
-                stopwatch.Start();               
-                IterativeSearch(numberList, key);
-                ticks += stopwatch.ElapsedTicks;
-            };
-            
-            Console.WriteLine("Benchmarking Iterative Search");
-            Console.WriteLine("*****************************");
-            double nanoseconds = (ticks / Stopwatch.Frequency) * 1000000000;
-            Console.WriteLine("{0:N2} ns: ", nanoseconds / n);          
-        }
-        
-        public static void BenchmarkRecursive(int[] arr, int key)
-        {
-            var stopwatch = new Stopwatch();
-            var n = 1000000;
-            var ticks = 0d;
-
-            for (int i = 0; i < n; i++)
-            {
-                stopwatch.Reset();
-                stopwatch.Start();
-                RecursiveBenchmark(arr, key, 0, arr.Length - 1);
-                ticks += stopwatch.ElapsedTicks;
-            };
-
-            Console.WriteLine("Benchmarking Recursive Search");
-            Console.WriteLine("*****************************");
-            double nanoseconds = (ticks / Stopwatch.Frequency) * 1000000000;
-            Console.WriteLine("{0:N2} ns: ", nanoseconds / n);
-        }
-
-        public static void RecursiveBenchmark(int[] array, int NumberToSearchFor, int min, int max)
+        static double RecursiveSearch(int[] array, int NumberToSearchFor, int min, int max)
         {
             NumberOfTimesCalled++;
             if (min > max)
             {
-                Console.WriteLine("min is bigger than max which is 0.");
+                return 0;
+                //return "Nil";
             }
             else
             {
                 int mid = (min + max) / 2;
-                if(NumberToSearchFor == array[mid])
+                if (NumberToSearchFor == array[mid])
                 {
-                }else if(NumberToSearchFor < array[mid])
+                    return ++mid;
+                }
+                else if (NumberToSearchFor < array[mid])
                 {
-                    RecursiveBenchmark(array, NumberToSearchFor, min, mid-1);
+                    return RecursiveSearch(array, NumberToSearchFor, min, mid - 1);
                 }
                 else
                 {
-                    RecursiveBenchmark(array, NumberToSearchFor, mid+1, max);
+                    return RecursiveSearch(array, NumberToSearchFor, mid + 1, max);
                 }
-            }  
+            }
+        }
+
+        static void Benchmark(string description, int iterations, Func<int[], int, double> method)
+        {
+            // eliminating overhead by GC by ensuring it has run before we start
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+
+
+            // warm up
+            // calling the function outside the timing loop so the overhead of the .NET JIT compiler isn't included
+            var temp = method(list, key);
+
+            // using stopwatch instead of DateTime.Now for a more accurate result
+            var watch = new Stopwatch();
+            watch.Start();
+
+            // unrolling the loop i.e doing multiple calls in the loop minimize the impact of the for loop computations
+            var loops = iterations / 10;
+            for (int i = 0; i < loops; i++)
+            {
+                // deadcode elimination
+                // store the result in a class variable to prevent the JIT compiler from optimizing it
+                result = method(list, key);
+                result = method(list, key);
+                result = method(list, key);
+                result = method(list, key);
+                result = method(list, key);
+                result = method(list, key);
+                result = method(list, key);
+                result = method(list, key);
+                result = method(list, key);
+                result = method(list, key);
+            }
+            watch.Stop();
+            Console.WriteLine("Benchmarking..." + description);
+            Console.WriteLine(
+                "{0:0.00} ms ({1:N0} ticks) (over {2:N0} iterations), {3:N0} ops/milliseconds",
+                watch.ElapsedMilliseconds, watch.ElapsedTicks, iterations,
+                (double)iterations / watch.ElapsedMilliseconds);
+        }
+
+        static void Benchmark(string description, int iterations, Func<int[], int, int, int, double> method)
+        {
+            // clean up
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+
+
+            // warm up
+            var temp = method(list, key, 0, 0);
+
+            var watch = new Stopwatch();
+            watch.Start();
+            var loops = iterations / 10;
+            for (int i = 0; i < loops; i++)
+            {
+                result = method(list, key, 0, 0);
+                result = method(list, key, 0, 0);
+                result = method(list, key, 0, 0);
+                result = method(list, key, 0, 0);
+                result = method(list, key, 0, 0);
+                result = method(list, key, 0, 0);
+                result = method(list, key, 0, 0);
+                result = method(list, key, 0, 0);
+                result = method(list, key, 0, 0);
+                result = method(list, key, 0, 0);
+            }
+            watch.Stop();
+            Console.WriteLine("Benchmarking... " + description);
+            Console.WriteLine(
+                "{0:0.00} ms ({1:N0} ticks) (over {2:N0} iterations), {3:N0} ops/milliseconds",
+                watch.ElapsedMilliseconds, watch.ElapsedTicks, iterations,
+                (double)iterations / watch.ElapsedMilliseconds);
         }
     }
 }
